771270ab142b22121cdc416f4cee859c20c98674
Garrett Wright <garrettwrong@gmail.com>
2021-06-10 08:07:43 -0400

    remove redundant attribute

--- src/aspire/source/image.py
+++ src/aspire/source/image.py
@@ -229,7 +229,6 @@ class ImageSource:
         self.set_metadata(
             ["_rlnAngleRot", "_rlnAngleTilt", "_rlnAnglePsi"], np.rad2deg(values)
         )
-        self._rotations_set = True
 
     @rots.setter
     def rots(self, values):
@@ -243,7 +242,6 @@ class ImageSource:
             ["_rlnAngleRot", "_rlnAngleTilt", "_rlnAnglePsi"],
             self._rotations.as_euler("ZYZ", degrees=True),
         )
-        self._rotations_set = True
 
     def set_metadata(self, metadata_fields, values, indices=None):
         """
